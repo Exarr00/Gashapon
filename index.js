639066e9e057b7099877f08b8bf6e930370a0807
Andy N <32938967+Exarr00@users.noreply.github.com>
2022-05-24 21:52:54 -0400

    update summoning to include characters

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
-import roll from './gacha.js'
+import gacha from './gacha.js';
+import characters from './characters.json' assert { type: 'json' };
 let history = [];
 
-const baseProbability = roll.generateRange(940, 53, 7);
-roll.testChance(baseProbability)
+console.log(characters);
+const baseProbability = gacha.generateRange(940, 53, 7);
+gacha.setGacha(characters);
+console.log(gacha.roll(baseProbability))
+console.log(gacha.multiRoll(baseProbability))
