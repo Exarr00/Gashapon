46dfdd98f45dc3d4fca7c0c9b43958eeb98e20f6
Andy N <32938967+Exarr00@users.noreply.github.com>
2022-06-08 23:07:33 -0400

    skip button styling

--- index.js
+++ index.js
@@ -180,6 +180,7 @@ const tosummon = () => {
 
 const gotosummon = () => {
   videoContainer.style.display = "none";
+  video.currentTime = 0; //mobile??
   console.log(getComputedStyle(container).display);
   container.style.display = "grid";
 };
