105d0d9303ab8c20b9b7aa757b644f15d4481af4
Alan Yuan <alan.yuan62@myhunter.cuny.edu>
2022-06-16 13:03:52 -0400

    moving around functions

--- history.js
+++ history.js
@@ -1,4 +1,6 @@
 const history = [];
+let historyList = document.getElementById("history-table");
+const pageNumber = document.getElementById("current-number");
 
 const getHistory = () => {
   return history;
@@ -17,4 +19,33 @@ const updateHistory = (...result) => {
   history.push(...the_history);
 };
 
-export default { getHistory, updateHistory };
+const changePage = () => {
+  let x = getHistory().slice(
+    (Number(pageNumber.textContent) - 1) * 7,
+    7 * Number(pageNumber.textContent)
+  );
+  while (historyList.childElementCount > 1) {
+    historyList.removeChild(historyList.lastChild);
+  }
+  x.forEach((historyItem) => {
+    let itemName = document.createElement("td");
+    let hiddenImage = document.createElement("img");
+    let itemDate = document.createElement("td");
+    let itemRating = document.createElement("td");
+    let tableRow = document.createElement("tr");
+    itemName.textContent = historyItem.name;
+    hiddenImage.setAttribute("class", "card-text-image");
+    hiddenImage.src = `./imgs/cards/${historyItem.name
+      .split(" ")
+      .join("_")}.png`;
+    itemDate.textContent = historyItem.date.toDateString();
+    itemRating.textContent = historyItem.rating;
+    itemName.append(hiddenImage);
+    tableRow.appendChild(itemName);
+    tableRow.appendChild(itemDate);
+    tableRow.appendChild(itemRating);
+    historyList.appendChild(tableRow);
+  });
+};
+
+export default { getHistory, updateHistory, changePage };
