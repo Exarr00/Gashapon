7994fbdf6ccb089dc28ac33bb3b52b6fa9a381a5
Alan Yuan <alan.yuan62@myhunter.cuny.edu>
2022-06-20 12:02:07 -0400

    fixed errors in history js

--- js/service.js
+++ js/service.js
@@ -1,5 +1,7 @@
 const getChar = () => {
-  return fetch('./characters.json').then((res) => res.json());
+  return fetch("./characters.json").then((res) => res.json());
 };
+
+const standardChar = () => {};
 
 export default getChar;
