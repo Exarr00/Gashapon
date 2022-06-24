8038f359388b651d329fec22c1c985f53e306da9
Andy N <32938967+Exarr00@users.noreply.github.com>
2022-06-23 21:26:57 -0400

    history rarity clarity + pity fix

--- js/history.js
+++ js/history.js
@@ -1,6 +1,6 @@
 const history = [];
-let historyList = document.getElementById("history-table");
-const pageNumber = document.getElementById("current-number");
+let historyList = document.getElementById('history-table');
+const pageNumber = document.getElementById('current-number');
 
 const getHistory = () => {
   const actualHistory = history.reverse();
@@ -38,34 +38,34 @@ const updateHistory = (...result) => {
 };
 
 const addListeners = () => {
-  document.querySelectorAll("#historyImageSmall").forEach((item) => {
-    item.addEventListener("click", (item2) => {
+  document.querySelectorAll('#historyImageSmall').forEach((item) => {
+    item.addEventListener('click', (item2) => {
       if (item2.target.nextSibling) {
-        item2.target.nextSibling.style.display = "block";
+        item2.target.nextSibling.style.display = 'block';
       }
     });
   });
-  document.querySelectorAll(".closeImage").forEach((closeItem) => {
-    closeItem.addEventListener("click", (closeItem2) => {
-      closeItem2.target.parentNode.style.display = "none";
+  document.querySelectorAll('.closeImage').forEach((closeItem) => {
+    closeItem.addEventListener('click', (closeItem2) => {
+      closeItem2.target.parentNode.style.display = 'none';
     });
   });
 };
 
 const subFunction = (cell, data) => {
-  let itemImageCell = document.createElement("td");
-  let imageSmall = document.createElement("img");
-  let imageLarge = document.createElement("img");
-  let imageModal = document.createElement("div");
-  let closeImage = document.createElement("span");
+  let itemImageCell = document.createElement('td');
+  let imageSmall = document.createElement('img');
+  let imageLarge = document.createElement('img');
+  let imageModal = document.createElement('div');
+  let closeImage = document.createElement('span');
 
-  imageSmall.src = `./imgs/cards/${data.name.split(" ").join("_")}.png`;
-  imageLarge.src = `./imgs/cards/${data.name.split(" ").join("_")}.png`;
-  imageLarge.setAttribute("id", "modal-content-img");
-  itemImageCell.setAttribute("id", "historyImageSmall");
-  imageModal.setAttribute("id", "modalImg");
-  closeImage.setAttribute("class", "closeImage");
-  closeImage.textContent = "X";
+  imageSmall.src = `./imgs/cards/${data.name.split(' ').join('_')}.png`;
+  imageLarge.src = `./imgs/cards/${data.name.split(' ').join('_')}.png`;
+  imageLarge.setAttribute('id', 'modal-content-img');
+  itemImageCell.setAttribute('id', 'historyImageSmall');
+  imageModal.setAttribute('id', 'modalImg');
+  closeImage.setAttribute('class', 'closeImage');
+  closeImage.textContent = 'X';
 
   imageModal.appendChild(closeImage);
   imageModal.appendChild(imageLarge);
@@ -77,6 +77,7 @@ const subFunction = (cell, data) => {
 };
 
 const changePage = () => {
+  const rarity = { '3': 'bronzeHistory', '4': 'silverHistory', '5': 'goldHistory' };
   let x = getHistory().slice(
     (Number(pageNumber.textContent) - 1) * 7,
     7 * Number(pageNumber.textContent)
@@ -85,10 +86,11 @@ const changePage = () => {
     historyList.removeChild(historyList.lastChild);
   }
   x.forEach((historyItem) => {
-    let itemName = document.createElement("td");
-    let itemDate = document.createElement("td");
-    let itemRating = document.createElement("td");
-    let tableRow = document.createElement("tr");
+    let itemName = document.createElement('td');
+    let itemDate = document.createElement('td');
+    let itemRating = document.createElement('td');
+    let tableRow = document.createElement('tr');
+    tableRow.classList.add(rarity[String(historyItem.rating)])
     itemName.textContent = historyItem.name;
     itemDate.textContent = historyItem.date.toDateString();
     itemRating.textContent = historyItem.rating;
